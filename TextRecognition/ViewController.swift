26634c474073aa788d8274c9b6c236e4838c014a
Dilbar Isakova <geekwarns@gmail.com>
2022-09-12 23:38:12 +0600

    add project

---
+++ TextRecognition/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by eesaack on 12.09.2022.
+//
+
+import Vision
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.backgroundColor = .green
+        label.text = "Starting..."
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "example1")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(label)
+        view.addSubview(imageView)
+        
+        recognizeText(image: imageView.image)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(x: 20,
+                                 y: view.safeAreaInsets.top,
+                                 width: view.frame.size.width-40,
+                                 height: view.frame.size.width-40)
+        label.frame = CGRect(x: 20,
+                             y: view.frame.size.width + view.safeAreaInsets.top,
+                             width: view.frame.size.width-40,
+                             height: 200)
+    }
+
+    private func recognizeText(image: UIImage?) {
+        guard let cgImage = image?.cgImage else {
+            fatalError("could not get cgimage")
+            
+        }
+        
+        //Handler
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        
+        //Request
+        let request = VNRecognizeTextRequest { [weak self] request, error in
+            //print(request.results)
+            guard let observations = request.results as? [VNRecognizedTextObservation],
+                  error == nil else {
+                //DispatchQueue.main.async {
+                //    self?.label.text = "\(error)"
+               // }
+                return
+            }
+            
+            let text = observations.compactMap({
+                $0.topCandidates(1).first?.string
+            }).joined(separator: ",")
+            
+            DispatchQueue.main.async {
+                self?.label.text = text
+            }
+        }
+        
+        //Process request
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            label.text = "\(error)"
+        }
+    }
+}
+
